a72505a55dccf7d5b90632b23f84620042a53adf
Liz Baron <10554+lizbaron@users.noreply.github.com>
2020-12-20 08:18:11 -0500

    Update manualPrep.ps1

--- envSetup/manualPrep.ps1
+++ envSetup/manualPrep.ps1
@@ -3,4 +3,6 @@ $projectName = "db-cicd-with-github-actions"
 $azResourceGroupName = "rg_" + $projectName
 $azServicePrincipalName = "sp_" + $projectName
 
-az ad sp create-for-rbac -n "$azServicePrincipalName" --role contributor --scopes "/subscriptions/$azSubscriptionId/resourceGroups/$azResourceGroupName"
+$spCredential = az ad sp create-for-rbac -n "$azServicePrincipalName" --role contributor --scopes "/subscriptions/$azSubscriptionId/resourceGroups/$azResourceGroupName"
+
+# Now print out $spCredential (it's a json snippet, fyi) and save it in your secrets.
