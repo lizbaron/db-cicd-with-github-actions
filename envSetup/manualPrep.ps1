3e6442ed772c176807fbf86379187881131ab571
Liz Baron <10554+lizbaron@users.noreply.github.com>
2020-12-20 08:33:30 -0500

    Update manualPrep.ps1
    Where did  --sdk-auth go??

--- envSetup/manualPrep.ps1
+++ envSetup/manualPrep.ps1
@@ -8,6 +8,6 @@ $azServicePrincipalName = "sp_" + $projectName
 $rgDetails = az group create -l $region -n $azResourceGroupName
 # Pro Tip: Make sure that Provisioning State is Suceeded.
 
-$spCredential = az ad sp create-for-rbac -n "$azServicePrincipalName" --role contributor --scopes "/subscriptions/$azSubscriptionId/resourceGroups/$azResourceGroupName"
+$spCredential = az ad sp create-for-rbac -n "$azServicePrincipalName" --sdk-auth --role contributor --scopes "/subscriptions/$azSubscriptionId/resourceGroups/$azResourceGroupName" 
 
 # Now print out $spCredential (it's a json snippet, fyi) and save it in your secrets.
