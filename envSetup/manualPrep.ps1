d96b9c9e6f43c13d27530860f84c9cd9e52c95ba
Liz Baron <10554+lizbaron@users.noreply.github.com>
2020-12-20 08:26:08 -0500

    Update manualPrep.ps1

--- envSetup/manualPrep.ps1
+++ envSetup/manualPrep.ps1
@@ -1,7 +1,12 @@
+
 $azSubscriptionId = "xxx-xxx-xxx"
+$region = "eastus2"
 $projectName = "db-cicd-with-github-actions"
 $azResourceGroupName = "rg_" + $projectName
 $azServicePrincipalName = "sp_" + $projectName
+
+$rgDetails = az group create -l $region -n $azResourceGroupName
+# Pro Tip: Make sure that Provisioning State is Suceeded.
 
 $spCredential = az ad sp create-for-rbac -n "$azServicePrincipalName" --role contributor --scopes "/subscriptions/$azSubscriptionId/resourceGroups/$azResourceGroupName"
 
