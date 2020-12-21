7de2626f934c28f928e910d7d3140c3f48b01873
Liz Baron <10554+lizbaron@users.noreply.github.com>
2020-12-21 07:13:34 -0500

    Mostly clean up, but also some add'l resources

--- envSetup/setup.ps1
+++ envSetup/setup.ps1
@@ -11,13 +11,13 @@ function Get-RandomCharacters($length, $characters) {
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
+$region = (Get-AzResourceGroup -Name $azResourceGroupName).Location
 $azResourceGroupName = "rg_" + $projectName;
 $azSecretsManagerName = "sm_" + $projectName;
 $aksClusterName = "aks_" + $projectName;
 $containerRegistryName = "crn_" + $projectName;
 $aksWinUser = "aksWinUser_" + (Get-RandomCharacters -length 10 -characters '1234567890')
 $aksWinNodePoolName = "aksWinNodePool_" + (Get-RandomCharacters -length 10 -characters '1234567890')
-$region = (Get-AzResourceGroup -Name $azResourceGroupName).Location
 
 Write-Debug ("Project Name: {0}" -f "$projectName"); 
 Write-Debug ("Region: {0}" -f "$region"); 
@@ -28,16 +28,11 @@ Write-Debug ("Container Registry Name: {0}" -f "$containerRegistryName");
 Write-Debug ("AKS Win User Name: {0}" -f "$aksWinUser"); 
 Write-Debug ("AKS Win Node Pool Name: {0}" -f "$aksWinNodePoolName"); 
 
-
 # Set up Secrets Manager on Azure (AKV)
 New-AzKeyVault -VaultName "$azSecretsManagerName" -ResourceGroupName "$azResourceGroupName" -Location "$region"
 
-# Add required secrets
-# $resourceGroupName
-# $region
-
-
 $aksPassword = ConvertTo-SecureString -String (Get-RandomCharacters -length 40 -characters 'abcdefghiklmnoprstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ') -AsPlainText -Force
+
 Set-AzKeyVaultSecret -VaultName "$azSecretsManagerName" -Name 'aksPassword' -SecretValue $aksPassword;
 Set-AzKeyVaultSecret -VaultName "$azSecretsManagerName" -Name 'projectName' -SecretValue $projectName;
 Set-AzKeyVaultSecret -VaultName "$azSecretsManagerName" -Name 'region' -SecretValue $region;
@@ -47,3 +42,13 @@ Set-AzKeyVaultSecret -VaultName "$azSecretsManagerName" -Name 'aksClusterName' -
 Set-AzKeyVaultSecret -VaultName "$azSecretsManagerName" -Name 'containerRegistryName' -SecretValue $containerRegistryName;
 Set-AzKeyVaultSecret -VaultName "$azSecretsManagerName" -Name 'aksWinUser' -SecretValue $aksWinUser;
 Set-AzKeyVaultSecret -VaultName "$azSecretsManagerName" -Name 'aksWinNodePoolName' -SecretValue $aksWinNodePoolName;
+
+# Create a Container Registry
+New-AzContainerRegistry -ResourceGroupName "$azResourceGroupName" -Name "$containerRegistryName" -Sku "Basic"
+
+# Create a new AKS Cluster with a single linux node
+New-AzAKS -ResourceGroupName "$resourceGroupName" -Name "$aksClusterName" -NodeCount 1 -KubernetesVersion 1.16.7 -NetworkPlugin azure -NodeVmSetType VirtualMachineScaleSets -WindowsProfileAdminUserName "$aksWinUser" -WindowsProfileAdminUserPassword "$aksPassword"
+
+# Add a Windows Server node pool to our existing cluster
+New-AzAksNodePool -ResourceGroupName "$resourceGroupName" -ClusterName "$aksClusterName" -OsType Windows -Name "$aksWinNodePoolName" -KubernetesVersion 1.16.7
+
