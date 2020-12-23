9923ca717b1d337587253dab0740f736280ea9f2
Liz Baron <10554+lizbaron@users.noreply.github.com>
2020-12-23 07:25:12 -0500

    protect from moving mice

--- envSetup/manualPrep.ps1
+++ envSetup/manualPrep.ps1
@@ -1,5 +1,5 @@
 
-$azSubscriptionId = "xxx-xxx-xxx"
+$azSubscriptionId = az account show --query id -o tsv
 $region = "eastus2"
 $projectName = "db-cicd-with-github-actions"
 $azResourceGroupName = "rg_" + $projectName
@@ -11,3 +11,11 @@ $rgDetails = az group create -l $region -n $azResourceGroupName
 $spCredential = az ad sp create-for-rbac -n "$azServicePrincipalName" --sdk-auth --role contributor --scopes "/subscriptions/$azSubscriptionId/resourceGroups/$azResourceGroupName" 
 
 # Now print out $spCredential (it's a json snippet, fyi) and save it in your secrets.
+
+az provider register --namespace 'Microsoft.KeyVault' 
+az provider register --namespace 'Microsoft.ContainerRegistry' 
+az provider register --namespace 'Microsoft.Kubernetes' 
+
+
+az provider list --query "[?contains('Microsoft.KeyVault|Microsoft.Kubernetes|Microsoft.ContainerRegistry',namespace)].{namespace: namespace, registrationState: registrationState}" -o table
+
