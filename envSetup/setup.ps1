163837b74a160defc75d24b393497830d7bc2464
Liz Baron <10554+lizbaron@users.noreply.github.com>
2020-12-23 21:24:50 -0500

    no digits?

--- envSetup/setup.ps1
+++ envSetup/setup.ps1
@@ -56,10 +56,11 @@ Set-AzKeyVaultAccessPolicy -VaultName "$azSecretsManagerName" -ObjectId $azServi
 #^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%\^&\*\(\)])[a-zA-Z\d!@#$%\^&\*\(\)]***12,123***$
 # TODO: ENSURE ^^^ 
 $part1 = (Get-RandomCharacters -length 5 -characters 'abcdefghiklmnoprstuvwxyz');
-$part2 = (Get-RandomCharacters -length 5 -characters '1234567890');
+#$part2 = (Get-RandomCharacters -length 5 -characters '1234567890');
 $part3 = (Get-RandomCharacters -length 10 -characters 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 $part4 = (Get-RandomCharacters -length 2 -characters '!#$%^&*');
-$allParts = -join ($part1,$part2,$part3,$part4);
+#$allParts = -join ($part1,$part2,$part3,$part4);
+$allParts = -join ($part1,$part3,$part4);
 $aksPassword = ConvertTo-SecureString -String "$allParts" -AsPlainText -Force
 
 Set-AzKeyVaultSecret -VaultName "$azSecretsManagerName" -Name 'aksPassword' -SecretValue $aksPassword;
